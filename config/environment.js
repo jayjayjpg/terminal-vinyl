1039cd51fe20ca20bd572730ad07bf3bdf07092d
Jessica Jordan <jessica.jordan.fe.dev@gmail.com>
2018-03-29 23:09:54 +0200

    vier

--- config/environment.js
+++ config/environment.js
@@ -44,6 +44,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
+    ENV.locationType = 'hash';
+    ENV.rootURL = '/terminal-vinyl/';
     // here you can enable a production-specific feature
   }
 
